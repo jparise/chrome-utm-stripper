3edb44bc897bbf5c16ed3a61939dcf07c6a534d7
Jon Parise <jon@indelible.org>
2012-07-03 18:04:09 -0700

    Update to manifest version 2.
    See http://code.google.com/chrome/extensions/manifestVersion.html
    
    We now package our JavaScript in its own standalone file.  Chrome is
    nice enough to generate a wrapping background.html-like container page
    for our automatically.
    
    Also, we now include a bit more helpful metadata in the manifest
    itself.
    
    Otherwise, no actually functionality has changed.

---
+++ background.js
@@ -0,0 +1,15 @@
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    var queryStringIndex = tab.url.indexOf('?');
+    if (tab.url.indexOf('utm_') > queryStringIndex) {
+        var stripped = tab.url.replace(
+            /([\?\&]utm_(source|medium|term|campaign|content)=[^&#]+)/ig,
+            '');
+        if (stripped.charAt(queryStringIndex) === '&') {
+            stripped = stripped.substr(0, queryStringIndex) + '?' +
+                stripped.substr(queryStringIndex + 1)
+        }
+        if (stripped != tab.url) {
+            chrome.tabs.update(tab.id, {url: stripped});
+        }
+    }
+});
