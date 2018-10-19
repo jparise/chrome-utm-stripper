888452a6158c62c388d3d337374e2170d68fedae
Elisha Eshed <elisha.e+git@gmail.com>
2018-10-19 14:03:17 +0300

    Block Facebook click ID parameter

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
-var utm_re = new RegExp('([\?\&]utm_(source|medium|term|campaign|content|cid|reader|name)=[^&#]+)', 'ig');
+var utm_re = new RegExp('([\?\&](fbclid|utm_(source|medium|term|campaign|content|cid|reader|name))=[^&#]+)', 'ig');
 
 chrome.webRequest.onBeforeRequest.addListener(function(details) {
     var url = details.url;
     var queryStringIndex = url.indexOf('?');
-    if (url.indexOf('utm_') > queryStringIndex) {
+    if (url.indexOf('utm_') > queryStringIndex || url.indexOf('fbclid') > queryStringIndex) {
         var stripped = url.replace(utm_re, '');
         if (stripped.charAt(queryStringIndex) === '&') {
             stripped = stripped.substr(0, queryStringIndex) + '?' +
