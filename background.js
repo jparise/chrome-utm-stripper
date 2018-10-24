1a3d99e70aeb2c60ac5762b04721c7348db78022
Elisha Eshed <elisha.e+git@gmail.com>
2018-10-24 18:46:29 +0300

    Remove Google's gclid

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
-var utm_re = new RegExp('([\?\&](fbclid|utm_(source|medium|term|campaign|content|cid|reader|name))=[^&#]+)', 'ig');
+var utm_re = new RegExp('([\?\&]((g|fb)clid|utm_(source|medium|term|campaign|content|cid|reader|name))=[^&#]+)', 'ig');
 
 chrome.webRequest.onBeforeRequest.addListener(function(details) {
     var url = details.url;
     var queryStringIndex = url.indexOf('?');
-    if (url.indexOf('utm_') > queryStringIndex || url.indexOf('fbclid') > queryStringIndex) {
+    if (url.indexOf('utm_') > queryStringIndex || url.indexOf('clid') > queryStringIndex) {
         var stripped = url.replace(utm_re, '');
         if (stripped.charAt(queryStringIndex) === '&') {
             stripped = stripped.substr(0, queryStringIndex) + '?' +
