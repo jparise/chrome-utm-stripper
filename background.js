33bbb031a72221b3eed09daf3db6ebd9ac56713c
Jon Parise <jon@indelible.org>
2019-05-24 12:12:32 -0700

    Strip ICID= query string parameters
    These are generally capitalized "in the wild", but we'll strip them
    case-insensitively.
    
    Closes #20

--- background.js
+++ background.js
@@ -3,7 +3,7 @@
  * parameter. We'll search the query string portion of the URL for this
  * pattern to determine if there's any stripping work to do.
  */
-var searchPattern = new RegExp('utm_|clid|mkt_tok', 'i');
+var searchPattern = new RegExp('utm_|clid|icid|mkt_tok', 'i');
 
 /*
  * Pattern matching the query string parameters (key=value) that will be
@@ -11,7 +11,7 @@ var searchPattern = new RegExp('utm_|clid|mkt_tok', 'i');
  */
 var replacePattern = new RegExp(
     '([?&]' +
-    '(mkt_tok|(g|fb)clid|utm_(source|medium|term|campaign|content|cid|reader|referrer|name))' +
+    '(icid|mkt_tok|(g|fb)clid|utm_(source|medium|term|campaign|content|cid|reader|referrer|name))' +
     '=[^&#]*)',
     'ig');
 
